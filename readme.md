59cf897dfc7da9c179306e2930fcad6c4ff0cc26
remco <rcvlierman@gmail.com>
2016-06-07 12:38:28 +0200

    initial commit and npm init

---
+++ readme.md
@@ -0,0 +1,16 @@
+# progalenor - Responive testing with protractor
+
+Note: this is very much work in progress.
+
+## What? 
+Progalenor is a helper page object to help you write cucumber (or old skool protrator jasmine) tests for pages with responsive behaviour.
+
+## Why?
+For responsive testing you can of course use galen. Although I really like this library I found it hard to fit in my build environment.
+Next to that, I didn't want to bore the devvers with yet another assertion library / syntax.
+
+## Usage
+will follow
+
+## examples
+will follow
---
+++ src/progalenor.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ *  This PO helps with responisiveness testing.
+ *  All comparator functions take 2 prostractor elements
+ * @constructor
+ */
+var RespPO = function () {
+
+};
+var breakpoints = [
+    { name: 'xs', start: 0 },
+    { name: 'sm', start: 768 },
+    { name: 'md', start:1024 },
+    { name: 'lg', start:1440 }
+];
+
+RespPO.prototype = Object.create({}, {
+
+    selectMediaQuery: {
+        value: function(mq) {
+            // set the window width in the center of a media query bandwidth
+            var width;
+            breakpoints.forEach(function(breakPoint, index) {
+               if( breakPoint.name === mq) {
+                   if(index < breakpoints.length-1) {
+                        width =  (breakPoint.start + breakpoints[index+1].start) / 2
+                   } else {
+                       width = breakPoint.start+200;
+                   }
+               }
+            });
+            return browser.manage().window().setSize(width, 480);
+        }
+    },
+    isBelow: {
+        value: function(element1, element2) {
+            return element1.getLocation().then(function(loc1) {
+                return element2.getLocation().then(function(loc2) {
+                    return elementHeight(element2).then(function(height) {
+                        return loc1.y < (loc2.y + height);
+                    });
+                });
+            });
+        }
+    },
+    isLeftOf: {
+        value: function(element1, element2) {
+            return element1.getLocation().then(function(loc1) {
+                return elementWidth(element1).then(function(width){
+                    return  element2.getLocation().then(function(loc2) {
+                        return loc1.x + width < loc2.x;
+                    });
+                });
+            });
+        }
+    },
+    hasWidth: {
+        value: function(element) {
+            return elementWidth(element);
+        }
+    },
+    hasHeight: {
+        value: function(element) {
+           return elementHeight(element);
+        }
+    }
+});
+
+function elementWidth(element) {
+    return element.getSize().then(function(size) {
+        return size.width;
+    });
+}
+
+function elementHeight(element) {
+    return element.getSize().then(function(size) {
+        return size.height;
+    });
+}
+
+module.exports = RespPO;
